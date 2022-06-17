e0379d912db9fff796080c767b256dae9059c96b
YossefHussein <youssef.hussein.business@gmail.com>
2022-06-17 15:32:24 +0200

    update

--- bin/classes_objects.dart
+++ bin/classes_objects.dart
@@ -1,10 +1,15 @@
 void main(List<String> args) {
-  Cars _cars = Cars();
-  _cars.printCarBrand();
+  // definition of object
+  Cars myCars = Cars();
+  // call method from class (Cars) by object myCars
+  myCars.printCarBrand();
 }
 
 class Cars {
   var civic = 'civic model 2012';
+
+  /// this is a method
+  /// method like function but it's created in class
 
   printCarBrand() {
     print('my car are $civic');
