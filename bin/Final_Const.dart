f3421c01e8a12fbf763de34b64767508692071ca
YossefHussein <youssef.hussein.business@gmail.com>
2022-06-17 14:57:52 +0200

    update

--- bin/Final_Const.dart
+++
@@ -1,8 +0,0 @@
-void main(List<String> args) {
-  /// Run in compiler time
-  const int myDegree = 89;
-
-  /// Work in run time
-  final String myName = 'Youssef Mohamed Ahmed Hussein';
-  print("'My name is $myName and my degree is $myDegree'");
-}
--- bin/Variables_and_int.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:async';
-
-void main(List<String> arguments) {
-  int firstNumber = 50;
-  int secondNumber = 25;
-  int degree = firstNumber + secondNumber;
-  print('your degree are $degree');
-}
