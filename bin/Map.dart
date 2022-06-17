2f5be907dfa0508760d07133b927b3679bd9c9d8
YossefHussein <youssef.hussein.business@gmail.com>
2022-06-17 15:00:49 +0200

    update

--- bin/Map.dart
+++
@@ -1,8 +0,0 @@
-void main(List<String> args) {
-  Map<String, String> myData = {
-    'name': 'ahmed',
-    'age': '25',
-    'number': '3449204',
-  };
-  print(myData);
-}
--- bin/String.dart
+++
@@ -1,4 +0,0 @@
-void main(List<String> args) {
-  String myName = 'Youssef Hussein';
-  print('Your name ($myName)');
-}
